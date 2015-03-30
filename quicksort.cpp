4821ab0eccffdda8d63e8be2f18c4d7a74b3b5d2
Hyoga708 <hyoga_quasar@yahoo.co.jp>
2015-03-31 08:23:49 +0900

    Create quicksort.cpp

---
+++ quicksort.cpp
@@ -0,0 +1,17 @@
+#include <iostream>
+using namespace std;
+#define	swap(a,b)	a^=b;	b^=a;	a^=b;
+
+void quicksort(int array[],int left,int right){
+	int moveleft=left,moveright=right;
+	int center=array[(left+right)/2]
+	while(1){
+		while(array[moveleft]<array[center])	moveleft++;
+		while(array[moveright]>array[center])	moveright++;
+		if(moveleft>=moveright)	break;
+		swap(array[moveleft],array[moveright]);
+		moveleft++;	moveright--;
+	}
+	if(moveleft-left>=2)	quicksort(array,left,moveleft-1);
+	if(right-moveright>=2)	quicksort(array,moveright+1,right);
+}
