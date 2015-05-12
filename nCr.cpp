e8157fe44608ad8031cbecf77b5e7c672de824c2
Hyoga708 <hyoga_quasar@yahoo.co.jp>
2015-05-12 21:41:44 +0900

    Create nCr.cpp

---
+++ nCr.cpp
@@ -0,0 +1,36 @@
+#include <iostream>
+using namespace std;
+#define mod 1000000007
+long long nCr(long long n,long long r);
+long long pow(long long a,long long b);
+
+int main(){
+	int n,r;	cin>>n>>r;
+	cout<<nCr(n,r)<<endl;
+	return 0;
+}
+
+long long nCr(long long n, long long r){ 
+  long long ret=1LL;
+  for(int i=1;i<=r;i++){
+    ret=(ret*((n-i+1)%mod))%mod;
+  }
+  for(int i=1;i<=r;i++){
+    ret=(ret*pow(i,mod-2))%mod;
+  }
+  return ret;
+}
+
+
+long long pow(long long a, long long b){ 
+  long long r = 1LL;
+
+  while(b){
+    if(b & 1LL)r *= a;
+    if(r >= mod)r %= mod;
+    a *= a;
+    if(a >= mod)a %= mod;
+    b >>= 1LL;
+  }
+  return r;
+}
